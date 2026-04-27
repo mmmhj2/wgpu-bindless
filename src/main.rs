ed8952822d176c8769245376827500b77a516949
Lee <vincent_li_2024@126.com>
2026-04-27 20:06:31 +0800

    Split app, state and renderer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+mod app;
 mod renderer;
 
 fn main() {
-    renderer::run();
+    app::run().unwrap();
 }
---
+++ src/renderer/window.rs
@@ -0,0 +1,94 @@
+
+use crate::renderer::device_interface::DeviceInterface;
+
+use std::sync::Arc;
+
+use winit::{
+    window::Window
+};
+
+pub struct State {
+    window: Arc<Window>,
+    device: DeviceInterface
+}
+
+impl State {
+    pub async fn new(window: Arc<Window>) -> Result<Self, ()> {
+        Ok(Self {
+            window: window.clone(),
+            device: DeviceInterface::new(window.clone()).await.unwrap()
+        })
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        self.device.create_surface(width, height);
+    }
+    
+    pub fn render(&mut self) -> Result<(), ()>{
+        self.window.request_redraw();
+
+        // We can't render unless the surface is configured
+        if !self.device.is_presentation_ready() {
+            return Ok(());
+        }
+            
+        let output = match self.device.get_current_surface_texture() {
+            wgpu::CurrentSurfaceTexture::Success(surface_texture) => surface_texture,
+            wgpu::CurrentSurfaceTexture::Suboptimal(surface_texture) => {
+                self.device.configure_surface();
+                surface_texture
+            }
+            wgpu::CurrentSurfaceTexture::Timeout
+            | wgpu::CurrentSurfaceTexture::Occluded
+            | wgpu::CurrentSurfaceTexture::Validation => {
+                // Skip this frame
+                return Ok(());
+            }
+            wgpu::CurrentSurfaceTexture::Outdated => {
+                self.device.configure_surface();
+                return Ok(());
+            }
+            wgpu::CurrentSurfaceTexture::Lost => {
+                // You could recreate the devices and all resources
+                // created with it here, but we'll just bail
+                panic!("Lost device");
+            }
+        };
+
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self.device.get_device().create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        // Do some draw call here
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    depth_slice: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+                multiview_mask: None,
+            });
+        }
+
+        // submit will accept anything that implements IntoIter
+        self.device.get_queue().submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
