9e40d4c322c62ce0b177d7e487bf74f5f7b254ee
Vincent Lee <vincent_li_2024@126.com>
2026-09-17 21:21:18 +0800

    Replace JSON serialization by Flexbuffer

--- src/asset/asset_types.rs
+++ src/asset/asset_types.rs
@@ -1,8 +1,9 @@
 pub mod static_mesh_asset;
 pub mod texture_asset;
 
-use std::fs::File;
+use std::{fs::File, io::{Read, Write}};
 
+use image::EncodableLayout;
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
 use crate::asset::{asset_types::{static_mesh_asset::StaticMeshAsset, texture_asset::TextureAsset}, importer::ImporterContext};
@@ -45,12 +46,14 @@ impl Asset {
     
     pub fn save_to_disk(&self) -> Result<(), ()> {
         let disk_path = self.metadata.asset_file_path.as_ref().expect("This asset cannot be saved to disk.");
-        serde_json::to_writer(File::create(disk_path).expect(""), self).expect("failed to serialize asset");
+        let b = flexbuffers::to_vec(self).unwrap();
+        File::create(disk_path).unwrap().write_all(b.as_bytes()).unwrap();
         Ok(())
     }
 
     pub fn load_from_disk(path: &std::path::Path) -> Result<Self, ()> {
-        let reader = File::open(path).expect("cannot open asset file");
-        Ok(serde_json::from_reader(reader).expect("invalid asset file"))
+        let mut buf = Vec::new();
+        File::open(path).expect("cannot open asset file").read_to_end(&mut buf).unwrap();
+        Ok(flexbuffers::from_buffer(&buf.as_slice()).unwrap())
     }
 }
